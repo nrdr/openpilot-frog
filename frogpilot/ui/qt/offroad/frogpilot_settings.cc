83074c2de6231dde274d3f719afaea5fd9fdd064
nrdr <81265253+nrdr@users.noreply.github.com>
2025-06-12 12:28:20 -0400

    lol

--- frogpilot/ui/qt/offroad/frogpilot_settings.cc
+++ frogpilot/ui/qt/offroad/frogpilot_settings.cc
@@ -140,8 +140,8 @@ FrogPilotSettingsWindow::FrogPilotSettingsWindow(SettingsWindow *parent) : QFram
                                           "../../frogpilot/assets/toggle_icons/icon_customization.png",
                                           togglePresets, true);
 
-  int timeTo100FPHours = 100 - (params_tracking.getInt("FrogPilotMinutes") / 60);
-  int timeTo250OPHours = 250 - (params.getInt("KonikMinutes") / 60) - (params.getInt("openpilotMinutes") / 60);
+  int timeTo100FPHours = 0 - (params_tracking.getInt("FrogPilotMinutes") / 60);
+  int timeTo250OPHours = 0 - (params.getInt("KonikMinutes") / 60) - (params.getInt("openpilotMinutes") / 60);
   togglePreset->setEnabledButtons(3, timeTo100FPHours <= 0 || timeTo250OPHours <= 0);
 
   QObject::connect(togglePreset, &FrogPilotButtonsControl::buttonClicked, [this](int id) {
