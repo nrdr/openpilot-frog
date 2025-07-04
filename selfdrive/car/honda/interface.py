d8610952024cfe70247469298675d0b05c69f1c6
nrdr <81265253+nrdr@users.noreply.github.com>
2025-07-04 16:41:04 -0400

    Update interface.py

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -101,15 +101,10 @@ class CarInterface(CarInterfaceBase):
         # ret.lateralParams.torqueV =   [0x0, 0x200, 0x300, 0x478, 0x5EC, 0x800, 0xA00, 0xE00, 0xF00] (hexdecimal)
         # ret.lateralParams.torqueBP = [0, 2327, 3525, 4119, 4511, 5131, 5760, 22464, 25344]
         # ret.lateralParams.torqueV = [0, 512, 768, 1144, 1516, 2048, 2560, 3584, 3840]
-        ret.lateralParams.torqueBP = [0, 2880, 5760, 8640, 11520, 14688, 17280, 20160, 23040]
-        ret.lateralParams.torqueV  = [0, 480, 960, 1440, 1920, 2448, 2880, 3360, 3840]
-        ret.lateralTuning.init('torque')
-        ret.lateralTuning.torque.useSteeringAngle = True
-        ret.lateralTuning.torque.kp = 1.0
-        ret.lateralTuning.torque.kf = 1.0
-        ret.lateralTuning.torque.ki = 0.1
-        ret.lateralTuning.torque.friction = 0.45
-        ret.lateralTuning.torque.latAccelFactor = 3.6
+        ret.lateralParams.torqueBP = [0x0, 0x917, 0xDC5, 0x1017, 0x119F, 0x140B, 0x1680, 0x69EF, 0x752F]
+        ret.lateralParams.torqueV  = [0x0, 0x200, 0x300, 0x478, 0x5EC, 0x800, 0xA00, 0xE00, 0xF00]
+        ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [[0, 10], [0.02750, 0.2750]]
+        ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [[0, 10], [0.00825, 0.0825]]
       else:
         ret.lateralTuning.pid.kf = 0.00006  # Default feed-forward
         ret.lateralParams.torqueBP = [0, 2560] # Stock Honda EPS Firmware
